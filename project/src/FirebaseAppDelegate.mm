2e5b5728fde5ce4f83a16d99948a69747fbd74eb
Evgeny Rovkin <evgeny.rovkin@nevosoft.ru>
2020-11-03 20:39:14 +0600

    upd

--- project/src/FirebaseAppDelegate.mm
+++ project/src/FirebaseAppDelegate.mm
@@ -3,20 +3,8 @@
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
 #import <Firebase.h>
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
+#import <FirebaseCrashlytics/FirebaseCrashlytics.h>
 
-// MESSAGING, disabled
-/*
-#import <FirebaseMessaging/FirebaseMessaging.h>
-
-// Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices running iOS 10 and above.
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-#import <UserNotifications/UserNotifications.h>
-@interface FirebaseAppDelegate () <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
-@end
-#endif
-*/
 
 
 @interface NMEAppDelegate : NSObject <UIApplicationDelegate>
@@ -29,12 +17,12 @@
 
 @implementation NMEAppDelegate(FirebaseAppDelegate)
 /*
-    -(BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *) launchOptions
-    {
-        [FIRApp configure];
-        NSLog(@"willFinishLaunchingWithOptions Firebase");
-        return YES;
-    }
+	-(BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *) launchOptions
+	{
+		[FIRApp configure];
+		NSLog(@"willFinishLaunchingWithOptions Firebase");
+		return YES;
+	}
  */
 @end
 
@@ -52,16 +40,16 @@ extern "C" void errorRemoteConfig(const char* messge);
   static FirebaseAppDelegate *_sharedInstance;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
-    _sharedInstance = [[self alloc] _init];
+	_sharedInstance = [[self alloc] _init];
   });
   return _sharedInstance;
 }
 
 - (instancetype)_init
 {
-  NSLog(@"FirebaseAppDelegate: _init");
-  [Fabric with:@[[Crashlytics class]]];
-  return self;
+	NSLog(@"FirebaseAppDelegate: _init");
+
+	return self;
 }
 
 - (instancetype)init
@@ -71,97 +59,103 @@ extern "C" void errorRemoteConfig(const char* messge);
 
 - (BOOL)setUserID:(NSString *)userID
 {
-    NSLog(@"FirebaseAppDelegate: setUserID id= %@", userID);
-    
-    [FIRAnalytics setUserID:userID];
-    return YES;
+	NSLog(@"FirebaseAppDelegate: setUserID id= %@", userID);
+	
+	[FIRAnalytics setUserID:userID];
+	return YES;
 }
 
 - (BOOL)setCrashlyticsUserID:(NSString *)userID
 {
-    NSLog(@"FirebaseAppDelegate: setCrashlyticsUserID id= %@", userID);
+	NSLog(@"FirebaseAppDelegate: setCrashlyticsUserID id= %@", userID);
 
-    [CrashlyticsKit setUserIdentifier:userID];
-    return YES;
+	[[FIRCrashlytics crashlytics] setUserID:userID];
+
+	return YES;
 }
 
 - (BOOL)setCurrentScreen:(NSString *)screenName screenClass:(NSString *)screenClass
 {
-    NSLog(@"FirebaseAppDelegate: setScreen name= %@, class= %@", screenName, screenClass);
-    
-    [FIRAnalytics setScreenName:screenName screenClass:screenClass];
-    return YES;
+	NSLog(@"FirebaseAppDelegate: setScreen name= %@, class= %@", screenName, screenClass);
+	
+	[FIRAnalytics setScreenName:screenName screenClass:screenClass];
+	//warning: 'setScreenName:screenClass:' is deprecated:
+	//Use +[FIRAnalytics logEventWithName:kFIREventScreenView parameters:] instead.
+
+	return YES;
 }
 
 - (BOOL)setUserProperty:(NSString *)propName propValue:(NSString *)propValue
 {
-    NSLog(@"FirebaseAppDelegate: setUserProperty key= %@, val= %@", propName, propValue);
-    
-    [FIRAnalytics setUserPropertyString:propValue forName:propName];
-    return YES;
+	NSLog(@"FirebaseAppDelegate: setUserProperty key= %@, val= %@", propName, propValue);
+	
+	[FIRAnalytics setUserPropertyString:propValue forName:propName];
+	return YES;
 }
 
 - (BOOL)setCrashlyticsProperty:(NSString *)propName propValue:(NSString *)propValue
 {
-    NSLog(@"FirebaseAppDelegate: setCrashlyticsProperty key= %@, val= %@", propName, propValue);
+	NSLog(@"FirebaseAppDelegate: setCrashlyticsProperty key= %@, val= %@", propName, propValue);
 
-    [CrashlyticsKit setObjectValue:propValue forKey:propName];
-    return YES;
+	[[FIRCrashlytics crashlytics] setCustomValue:propValue forKey:propName];
+
+	return YES;
 }
 
 - (BOOL)sendFirebaseAnalyticsEvent:(NSString *)eventName jsonPayload:(NSString *)jsonPayload
 {
-    NSLog(@"FirebaseAppDelegate: sendFirebaseAnalyticsEvent name= %@, parameter= %@", eventName, jsonPayload);
+	NSLog(@"FirebaseAppDelegate: sendFirebaseAnalyticsEvent name= %@, parameter= %@", eventName, jsonPayload);
 
-    NSData * jsonData = [jsonPayload dataUsingEncoding:NSUTF8StringEncoding];
-    NSError * error = nil;
-    NSDictionary * parsedData = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+	NSData * jsonData = [jsonPayload dataUsingEncoding:NSUTF8StringEncoding];
+	NSError * error = nil;
+	NSDictionary * parsedData = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
 
-    [FIRAnalytics logEventWithName:eventName parameters:parsedData];
-    return YES;
+	[FIRAnalytics logEventWithName:eventName parameters:parsedData];
+	return YES;
 }
 
 - (void)requestRemoteConfig
 {
-    //NSLog(@"extension_ios_firebase requestRemoteConfig OK");
-    
-    FIRRemoteConfig *remoteConfig = [FIRRemoteConfig remoteConfig];
-    FIRRemoteConfigSettings *remoteConfigSettings = [[FIRRemoteConfigSettings alloc] init];
-    remoteConfigSettings.minimumFetchInterval = 0;
-    remoteConfig.configSettings = remoteConfigSettings;
-    
-    [remoteConfig fetchWithCompletionHandler:^(FIRRemoteConfigFetchStatus status, NSError *error) {
-        if (status == FIRRemoteConfigFetchStatusSuccess) {
-            NSLog(@"Config fetched!");
-            [remoteConfig activateWithCompletionHandler:^(NSError * _Nullable error) {
-                //NSLog(@"---------------> WAITING FOR RESPONSE");
-                NSArray<NSString*>* keys = [remoteConfig allKeysFromSource:FIRRemoteConfigSourceRemote];
-                NSMutableDictionary* resultDict = [[NSMutableDictionary alloc] init];
-                for (id key in keys)
-                {
-                    //NSLog(key);
-                    NSString* strValue = [[remoteConfig configValueForKey:key] stringValue];
-                    //NSLog(strValue);
-                    [resultDict setValue:strValue forKey:key];
-                }
-                NSData *jsonData = [NSJSONSerialization dataWithJSONObject:resultDict options:NSJSONWritingPrettyPrinted error:&error];
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    NSString *resultString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-                    //NSLog(resultString);
-                    const char* responseChar = [resultString cStringUsingEncoding:[NSString defaultCStringEncoding]];
-                    responseRemoteConfig(responseChar);
-                });
-            }];
-        } else {
-            NSLog(@"Config not fetched");
-            NSLog(@"Error %@", error.localizedDescription);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                NSString *errorMessage = error.localizedDescription;
-                const char* responseChar = [errorMessage cStringUsingEncoding:[NSString defaultCStringEncoding]];
-                errorRemoteConfig(responseChar);
-            });
-        }
-    }];
+	//NSLog(@"extension_ios_firebase requestRemoteConfig OK");
+	
+	FIRRemoteConfig *remoteConfig = [FIRRemoteConfig remoteConfig];
+	FIRRemoteConfigSettings *remoteConfigSettings = [[FIRRemoteConfigSettings alloc] init];
+	remoteConfigSettings.minimumFetchInterval = 0;
+	remoteConfig.configSettings = remoteConfigSettings;
+	
+	[remoteConfig fetchWithCompletionHandler:^(FIRRemoteConfigFetchStatus status, NSError *error) {
+		if (status == FIRRemoteConfigFetchStatusSuccess) {
+			NSLog(@"Config fetched!");
+			
+			[remoteConfig activateWithCompletion:^(BOOL changed, NSError * _Nullable error) {
+				//NSLog(@"---------------> WAITING FOR RESPONSE");
+				NSArray<NSString*>* keys = [remoteConfig allKeysFromSource:FIRRemoteConfigSourceRemote];
+				NSMutableDictionary* resultDict = [[NSMutableDictionary alloc] init];
+				for (id key in keys)
+				{
+					//NSLog(key);
+					NSString* strValue = [[remoteConfig configValueForKey:key] stringValue];
+					//NSLog(strValue);
+					[resultDict setValue:strValue forKey:key];
+				}
+				NSData *jsonData = [NSJSONSerialization dataWithJSONObject:resultDict options:NSJSONWritingPrettyPrinted error:&error];
+				dispatch_async(dispatch_get_main_queue(), ^{
+					NSString *resultString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+					//NSLog(resultString);
+					const char* responseChar = [resultString cStringUsingEncoding:[NSString defaultCStringEncoding]];
+					responseRemoteConfig(responseChar);
+				});
+			}];
+		} else {
+			NSLog(@"Config not fetched");
+			NSLog(@"Error %@", error.localizedDescription);
+			dispatch_async(dispatch_get_main_queue(), ^{
+				NSString *errorMessage = error.localizedDescription;
+				const char* responseChar = [errorMessage cStringUsingEncoding:[NSString defaultCStringEncoding]];
+				errorRemoteConfig(responseChar);
+			});
+		}
+	}];
 }
 
 @end
